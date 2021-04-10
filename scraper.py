4ecb9e231dcf58075e74770814c3e66fd479d406
DylanScottCarroll <dyscarroll@gmail.com>
2021-04-10 14:14:29 -0700

    started working on scraper

--- scraper.py
+++ scraper.py
@@ -1,4 +1,6 @@
 from bs4 import BeautifulSoup
+import re
+from geopy.geocoders import Nominatim
 
 
 #Search Parameters
@@ -31,7 +33,19 @@ class Scraper:
         """Uses the list of parameters to search on yelp for a list of restaurants
         returns a list of dictionaries with restaurants that fit the criteria."""
         
-        pass
+        url = _assemble_yelp_url(self._parameters)
+        
+        address = _parameters["location"]
+        print(coordinates_from_address(address), 5)
+
+        _fetch_html(url)
+
+def coordinates_from_address(address, radius):
+    """Given the address and the radius, returns pair of coordinates forming a square that bounds that area"""
+    nominatim = Nominatim()
+    location = nominatim.geocode(address)
+    center = (location.latitude, location.longitude)
+
 
 def _assemble_yelp_url(parameters):
     """"Uses the dict of parameters to create the necessary yelp url"""
@@ -50,7 +64,13 @@ def _parse_restaurant(html):
     pass
 
 def _check_restaurant(restaurant, parameters):
+    """Checks that a restaurant conforms to the search parameters
+    Returns true if it does and false if it does not"""
     pass
 
 if __name__ == "__main__":
     print("Testing:")
+
+    scraper = Scraper()
+
+    scraper.search()
